80b4fe014e446edf821438c821408dc8809971bc
Matan Moshe <matanmoshe@Matans-MacBook-Pro.local>
2023-10-22 19:19:45 +0300

    final changes with comment

--- functions.py
+++ functions.py
@@ -1,18 +1,16 @@
-FILEPATH = "todos.txt"
+FILEPATH = "todos.txt"  # Defining the default file path for the to-do list.
 
-def get_todos(filepath=FILEPATH):
+def get_todos(filepath=FILEPATH):  # Function to read to-dos from a file; accepts a file path, defaults to FILEPATH.
     """ Read a text file and return the list of to-do items."""
+    with open(filepath, 'r') as file_local:  # Opening the file in read mode.
+        todos_local = file_local.readlines()  # Reading all lines from the file into a list.
+    return todos_local  # Returning the list of to-dos.
 
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-def write_todos(todos_local, filepath=FILEPATH):
+def write_todos(todos_local, filepath=FILEPATH):  # Function to write to-dos to a file; accepts a list and a file path, defaults to FILEPATH.
     """ Write the to-do items list in the text file. """
-    
-    with open(filepath, 'w') as file:
-            file.writelines(todos_local)
+    with open(filepath, 'w') as file:  # Opening the file in write mode.
+        file.writelines(todos_local)  # Writing the list of to-dos to the file.
 
-if __name__ == "__main__":
-     print("Hello")
-     print(get_todos())
+if __name__ == "__main__":  # Standard boilerplate to call the main() function to begin the program.
+    print("Hello")  # Printing a greeting message.
+    print(get_todos())  # Printing the list of to-dos retrieved from the file.
--- web.py
+++ web.py
@@ -1,31 +1,25 @@
-import streamlit as st
-import functions
+import streamlit as st                # Importing streamlit module for creating and running the web application.
+import functions                      # Importing custom functions for handling to-do operations.
 
-todos = functions.get_todos()
+todos = functions.get_todos()         # Retrieving the list of to-dos from the functions module.
 
-def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
-    functions.write_todos(todos)
+def add_todo():                       # Defining a function to add a new to-do item.
+    todo = st.session_state["new_todo"] + "\n"  # Getting the new to-do from the session state and appending a newline.
+    todos.append(todo)                # Adding the new to-do to the list of to-dos.
+    functions.write_todos(todos)      # Saving the updated list of to-dos using a function from the functions module.
 
+st.title("My Todo App")               # Setting the title of the web application.
+st.subheader("This is my todo app.")  # Adding a subheader to the web application.
+st.write("Thus app is to increase your productivity.")  # Displaying a message on the web application.
 
-st.title("My Todo App")
-st.subheader("This is my todo app.")
-st.write("Thus app is to increase your productivity.")
-
-
-for index, todo in enumerate(todos):
-    checkbox = st.checkbox(todo, key=f"todo {index}")
-    if checkbox:
-        todos.pop(index)
-        functions.write_todos(todos)
+for index, todo in enumerate(todos):  # Iterating over the list of to-dos with their indices.
+    checkbox = st.checkbox(todo, key=f"todo {index}")  # Creating a checkbox for each to-do item.
+    if checkbox:                      # Checking if the checkbox is marked (to-do is completed).
+        todos.pop(index)              # Removing the completed to-do from the list.
+        functions.write_todos(todos)  # Saving the updated list after removal of the completed to-do.
         #del st.session_state[todo]
-        st.experimental_rerun()
-#for index, todo in enumerate(todos):
-#    st.checkbox(todo, key=f"todo_{index}") 
+        st.experimental_rerun()       # Rerunning the app to update the list of to-dos displayed.
 
-
-st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key='new_todo')
-
+st.text_input(label="", placeholder="Add new todo...",  # Creating an input field for adding new to-dos.
+              on_change=add_todo, key='new_todo')       # Calling the add_todo function when the input value changes.
 
